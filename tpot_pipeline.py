2de0e6c4df8091c8b84fcc6686bfea802e4b6405
rushdanajeeb <rushdanajeeb@gmail.com>
2023-04-23 09:09:50 -0400

    added eda steps, correlation plot,limitations of model and reference link

--- tpot_pipeline.py
+++ tpot_pipeline.py
@@ -1,9 +1,8 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import AdaBoostRegressor, ExtraTreesRegressor
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import PolynomialFeatures
 from tpot.builtins import StackingEstimator
 from tpot.export_utils import set_param_recursive
 
@@ -13,11 +12,10 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: -0.12004812826210591
+# Average CV score on the training set was: 0.8372946859903381
 exported_pipeline = make_pipeline(
-    PolynomialFeatures(degree=2, include_bias=False, interaction_only=False),
-    StackingEstimator(estimator=AdaBoostRegressor(learning_rate=0.5, loss="square", n_estimators=100)),
-    ExtraTreesRegressor(bootstrap=True, max_features=0.3, min_samples_leaf=4, min_samples_split=15, n_estimators=100)
+    StackingEstimator(estimator=GradientBoostingClassifier(learning_rate=1.0, max_depth=5, max_features=0.45, min_samples_leaf=20, min_samples_split=10, n_estimators=100, subsample=0.5)),
+    RandomForestClassifier(bootstrap=True, criterion="gini", max_features=0.8, min_samples_leaf=15, min_samples_split=18, n_estimators=100)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
